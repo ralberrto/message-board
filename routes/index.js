6673802153100a521520b5a24eca63ba432b40cb
Rafael Alberto Larios <albertolarios@me.com>
2022-12-17 20:44:40 -0600

    Dinamically display messages in array with pug and style them

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Armando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello, world!',
+    user: 'Charlie',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages: messages });
 });
 
 module.exports = router;
